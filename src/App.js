9b56c34dd646aa92c1ddb5b292e28d56158da88e
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2022-06-04 23:00:49 -0300

    create nav component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <NavBar/>
+        <h1>Tienda Amigurumi</h1>
+      </header>
       Primer proyecto - Fernando Armonelli
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+export default function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li>Categoria A</li>
+                <li>Categoria B</li>
+                <li>Categoria C</li>
+            </ul>
+        </nav>
+    );
+}
