ab3ce58a390f4bcd0b8ca4dac9711194221c1756
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2022-06-09 23:53:01 -0300

    restore css and remove call to library

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import FontAwesome from 'react-fontawesome';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
